5ab854c51a2e1f38ce1a267addfddcce7f80887b
alcantara-samuel <you@example.com>
2023-03-25 09:08:42 -0300

    modified pages

--- assests/js/classes.js
+++ assests/js/classes.js
@@ -4,7 +4,8 @@ let menu = [
     {id: 3, nome: 'X-Salada', price: 8.00, img: '../img/cardapio/bacon.jpg',  desc: 'Pão, mussarela, presunto, tomate e alface' },
     {id: 4, nome: 'X-Egg', price: 13.00, img: '../img/cardapio/bacon.jpg',  desc: 'Pão, presunto, queijo prato, ovo, tomate e alface' },
     {id: 5, nome: 'X-Calabresa', price: 12.70, img: '../img/cardapio/bacon.jpg',  desc: 'Pão, calabresa,presunto, queijo prato, tomate e alface' },
-    {id: 6, nome: 'X-tudo', price: 16.00, img: '../img/cardapio/bacon.jpg',  desc: 'Pão, frango, carne, ovo, presunto, batata palha, salsisha, queijo prato, tomate e alface' },
-    {id: 7, nome: 'X-bacon crocante', price: 14.50, img: '../img/cardapio/bacon.jpg',  desc: 'Pão, bacon, presunto, batata palha, mussalera, tomate e alface' }
+    {id: 6, nome: 'X-Tudo', price: 16.00, img: '../img/cardapio/bacon.jpg',  desc: 'Pão, frango, carne, ovo, presunto, batata palha, salsisha, queijo prato, tomate e alface' },
+    {id: 7, nome: 'X-Bacon crocante', price: 14.50, img: '../img/cardapio/bacon.jpg',  desc: 'Pão, bacon, presunto, batata palha, mussalera, tomate e alface' },
+    {id: 8, nome: 'X-Frango Bacon', price: 14.50, img: '../img/cardapio/bacon.jpg',  desc: 'Pão, bacon, frango, batata palha, mussalera, tomate e alface' }
 
 ]
